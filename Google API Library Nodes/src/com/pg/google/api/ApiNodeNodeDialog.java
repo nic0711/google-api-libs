41f7a32664e1206e35b5a1a2ae5e2e02681d466c
ckevinhill <hill.ck@pg.com>
2015-02-12 10:40:07 -0500

    Initial import

---
+++ Google API Library Nodes/src/com/pg/google/api/ApiNodeNodeDialog.java
@@ -0,0 +1,25 @@
+package com.pg.google.api;
+
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+
+/**
+ * <code>NodeDialog</code> for the "ApiNode" Node.
+ * 
+ *
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more 
+ * complex dialog please derive directly from 
+ * {@link org.knime.core.node.NodeDialogPane}.
+ * 
+ * @author Procter & Gamble, eBusiness
+ */
+public class ApiNodeNodeDialog extends DefaultNodeSettingsPane {
+
+    /**
+     * New pane for configuring the ApiNode node.
+     */
+    protected ApiNodeNodeDialog() {
+
+    }
+}
+
---
+++ Google API Library Nodes/src/com/pg/google/api/ApiNodeNodeView.java
@@ -0,0 +1,48 @@
+package com.pg.google.api;
+
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeView</code> for the "ApiNode" Node.
+ * 
+ *
+ * @author Procter & Gamble, eBusiness
+ */
+public class ApiNodeNodeView extends NodeView<ApiNodeNodeModel> {
+
+    /**
+     * Creates a new view.
+     * 
+     * @param nodeModel The model (class: {@link ApiNodeNodeModel})
+     */
+    protected ApiNodeNodeView(final ApiNodeNodeModel nodeModel) {
+        super(nodeModel);
+        // TODO: generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void modelChanged() {
+        // TODO: generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onClose() {
+        // TODO: generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onOpen() {
+        // TODO: generated method stub
+    }
+
+}
+
